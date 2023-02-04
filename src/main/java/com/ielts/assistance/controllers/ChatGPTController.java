9a88c382661d43fdbddf4ebef42428df61b2034f
wrangzii <duynh7401@gmail.com>
2023-02-04 19:51:19 +0700

    init chatgpt

--- src/main/java/com/ielts/assistance/controllers/ChatGPTController.java
+++ src/main/java/com/ielts/assistance/controllers/ChatGPTController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin
 public class ChatGPTController {
     private final ChatgptService chatgptService;
 
