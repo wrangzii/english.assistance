7dcba019166fc469614432c79e411f6bf54fb479
wrangzii <duynh7401@gmail.com>
2023-02-19 18:55:40 +0700

    fix requesst body apis

--- chatGPT/src/main/java/io/github/chatgpt/service/ChatgptService.java
+++ chatGPT/src/main/java/io/github/chatgpt/service/ChatgptService.java
@@ -1,8 +1,5 @@
 package io.github.chatgpt.service;
 
-import io.github.chatgpt.dto.ChatRequest;
-import io.github.wrangz.chatgpt.dto.ChatResponse;
-
 public interface ChatgptService {
 
     String sendMessage(String message);
